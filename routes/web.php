20b2a5bc7f9266b04e515cbf037a7f870d620958
moinul <moinulibr@gmail.com>
2026-04-26 23:09:26 +0600

    redis setup

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Facades\Cache;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,4 +21,37 @@ Route::prefix('users')->group(function () {
     Route::get('/{identifier}', [UserController::class, 'show'])->name("user.show"); // Read
     Route::put('/update/{id}', [UserController::class, 'update'])->name("user.update");  // Update
     Route::delete('/delete/{id}', [UserController::class, 'destroy'])->name("user.delete"); // Delete
+});
+
+
+Route::get('/redis-test', function () {
+    // ১. সরাসরি রেডিস মেথড ব্যবহার (String)
+    Redis::set('moinul:skill', 'Mastering Redis');
+
+    // ২. লারাভেল ক্যাশ মেথড ব্যবহার (এটিও রেডিসে যাবে কারণ আমরা .env তে ক্যাশ রেডিস করেছি)
+    Cache::put('project_name', 'PureOlaa - Organic Food', 600); // ১০ মিনিট থাকবে
+
+    // ৩. ইনক্রিমেন্ট টেস্ট (লাইভ কাউন্টার)
+    $views = Redis::incr('live_visitor_count');
+
+    // ১. স্ট্রিং ডেটা (এটি দিয়ে তুমি নাম বা ছোট তথ্য সেভ করবে)
+    Redis::set('user:1:name', 'Moinul - Developer');
+
+    // ২. সেশন বা টেম্পোরারি ডেটা (৬০ সেকেন্ড পর ভ্যানিশ হয়ে যাবে)
+    Redis::setex('session:token', 60, 'ABC-123-XYZ');
+
+    // ৩. ইনক্রিমেন্ট (লাইক বা ভিউ কাউন্টার)
+    // প্রতিবার রিফ্রেশ করলে এটি ১ করে বাড়বে
+    Redis::incr('post:99:views');
+
+    // ৪. লিস্ট (কিউ বা মেসেজ পাঠানোর জন্য)
+    Redis::rpush('notifications', 'আপনার প্রোফাইল কেউ ভিজিট করেছে!');
+    Redis::rpush('notifications', 'নতুন একটি মেসেজ এসেছে।');
+
+    // ৫. হ্যাশ (পুরো একটা অবজেক্ট বা ইউজারের ডিটেইলস)
+    Redis::hset('pureolaa:product:1', 'name', 'Pure Honey');
+    Redis::hset('pureolaa:product:1', 'price', '500');
+    Redis::hset('pureolaa:product:1', 'stock', '25');
+    
+    return "সফলভাবে ডেটা রেডিসে পাঠানো হয়েছে। বর্তমান ভিউ সংখ্যা: " . $views;
 });
